c8ed33491d4372f654e68e614edf68dfa45a6aa8
Rebel <rebel@Rebels-MacBook-Pro.local>
2020-11-17 19:03:05 +0530

    updated gitignore and added setup file

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# run this script to setup chat-app project(client + server)
+
+server_folder="server"
+client_folder="client"
+
+working_dir="/Users/rebel/Documents/personal/code/chat-app"
+cd $working_dir
+echo "Entered "$working_dir
+
+cd $server_folder
+rm -rf node_modules
+npm i
+
+echo "Server Setup is done!!"
+
+cd $working_dir
+cd $client_folder
+rm -rf node_modules
+npm i
+
+echp "Client Setup is done!!"
+
