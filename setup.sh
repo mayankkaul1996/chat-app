0ee88b52f5eb1285d1882438cfe754d68c770d3f
Rebel <rebel@Rebels-MacBook-Pro.local>
2020-11-17 19:07:11 +0530

    added comments

--- setup.sh
+++ setup.sh
@@ -9,16 +9,16 @@ working_dir="/Users/rebel/Documents/personal/code/chat-app"
 cd $working_dir
 echo "Entered "$working_dir
 
+# server setup
 cd $server_folder
 rm -rf node_modules
 npm i
-
 echo "Server Setup is done!!"
 
+# client setup
 cd $working_dir
 cd $client_folder
 rm -rf node_modules
 npm i
-
 echp "Client Setup is done!!"
 
