49b0e86c1b12c213422404bb628ae292eee63bae
Rebel <rebel@Rebels-MacBook-Pro.local>
2020-11-17 19:40:32 +0530

    changed css

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -71,9 +71,9 @@ const Chat = ({location}) => {
     return (
             <div className="outerContainer">
                 <div className="container">
-                    <InfoBar room={room}/>
-                    <Messages messages={messages} name={name} />
-                    <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+                    <InfoBar className="chatInfoFar" room={room}/>
+                    <Messages className="chatMessageSection"  messages={messages} name={name} />
+                    <Input className="chatInputBox"  message={message} setMessage={setMessage} sendMessage={sendMessage}/>
                 </div>
                 <TextContainer users={users}/>
             </div>
